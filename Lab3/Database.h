6cbdce55b997bce2add64bb34973521e65b2f2b2
VladimirDan <vovaoil2003@gmail.com>
2023-11-21 17:39:42 +0200

    Lab 3 initialization

---
+++ Lab3/Database.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Employee.h"
+#include <string>
+
+using namespace std;
+
+namespace Records {
+	const int kMaxEmployees = 100;
+	const int kFirstEmployeeNumber = 1000;
+	class Database
+	{
+	public:
+		Database();
+		~Database();
+		Employee& addEmployee(
+			string firstName,
+			string lastName,
+			string fathersName,
+			int age,
+			Sex sex,
+			string address,
+			int passportNumber,
+			PositionCode positionCode,
+			int mEmployeeCode);
+		Employee& getEmployee(int inEmployeeNumber);
+		Employee& getEmployee(std::string inFirstName, std::string inLastName);
+		void displayAll();
+		void displayCurrent();
+		void displayFormer();
+		void displayAdult();
+	protected:
+		Employee mEmployees[kMaxEmployees];
+		int mNextSlot;
+		int mNextEmployeeNumber;
+	};
+}
